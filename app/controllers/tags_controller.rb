c2adf0bc3ff33b9971f23c0f1b06a33c381cbb58
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-05 16:34:36 -0700

    Delete tag from server UI
    Co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>
    Co-authored-by: Dave Lindqvist <lindqvist.dave@gmail.com>

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -17,11 +17,27 @@ class TagsController < ApplicationController
     end
   end
 
+  def unjoin
+    @recipe = Recipe.find(params[:recipe_id])
+    @tag = Tag.find(params[:id])
+    @recipe.tags.delete(@tag)
+    # @tag.recipes.delete(@recipe)
+    redirect_to recipe_path(@recipe)
+  end
+
+
   def destroy
+    @recipe = Recipe.find(params[:recipe_id])
     @tag = Tag.find(params[:id])
     @tag.destroy
     flash[:notice] = "Tag successfully destroyed!"
-    redirect_to recipe_path(@tag.recipe)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def show
+    @recipe = Recipe.find(params[:recipe_id])
+    @tag = Tag.find(params[:id])
+    render :show
   end
 
   private
